9920c2e3ccc4ff6295c375587dbf36c6e95ec526
mukeshnambari <mukesh.nambari3@gmail.com>
2024-10-28 10:28:45 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "hashtek-terraform-state-bucket-230424"
-    key = "main"
-    region = "us-east-1"
-    dynamodb_table = "hashtekTerraformStateLock"
+    bucket = "tarunmukesh"
+    key = "path/terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "statelock"
   }
 }
